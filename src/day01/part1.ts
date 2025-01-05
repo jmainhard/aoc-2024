95032cb1cca82600e22637d683dde54d2c7df824
jmainhard <jmainhard@outlook.com>
2025-01-05 17:59:06 -0300

    refactor(day01): refactor list names on part 1

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -2,18 +2,18 @@
 
 export function part1(input: string): number {
   const lines = input.trim().split('\n');
-  const a: number[] = [];
-  const b: number[] = [];
+  const firstList: number[] = [];
+  const secondList: number[] = [];
   let totalDistance: number = 0;
   lines.forEach((line) => {
     const [x, y] = line.split(/\s+/).map(Number);
-    a.push(x);
-    b.push(y);
+    firstList.push(x);
+    secondList.push(y);
   });
-  a.sort();
-  b.sort();
+  firstList.sort();
+  secondList.sort();
   for (let i = 0; i < lines.length; i++) {
-    totalDistance += Math.abs(a[i] - b[i]);
+    totalDistance += Math.abs(firstList[i] - secondList[i]);
   }
   return totalDistance;
 }
