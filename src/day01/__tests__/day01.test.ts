6a90f6eb17ae1912fe82a9bd4a2fffaa4941105e
jmainhard <jmainhard@outlook.com>
2025-01-05 15:20:52 -0300

    day01: add part 1
    Add solution to day01 part 1.
    Add test for sample input part 1.

--- src/day01/__tests__/day01.test.ts
+++ src/day01/__tests__/day01.test.ts
@@ -1,15 +1,24 @@
 // Advent of Code - Day 1
 
-import { describe, expect, test } from 'vitest';
+import { beforeAll, describe, expect, test } from 'vitest';
 import { part1 } from '../part1.js';
 import { part2 } from '../part2.js';
+import { readFile } from 'fs/promises';
 
 describe('Day 1', () => {
+  let input: string;
+  beforeAll(async () => {
+    input = await readFile(
+      'src/day01/__tests__/day01.sample.txt',
+      'utf8',
+    );
+  });
+
   test('Part 1', () => {
-    expect(part1("")).toBe(0);
+    expect(part1(input)).toBe(11);
   });
 
   test('Part 2', () => {
-    expect(part2("")).toBe(0);
+    expect(part2('')).toBe(0);
   });
 });
--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -1,5 +1,19 @@
 // Advent of Code - Day 1 - Part One
 
 export function part1(input: string): number {
-  return 0;
+  const lines = input.trim().split('\n');
+  const a: number[] = [];
+  const b: number[] = [];
+  let totalDistance: number = 0;
+  lines.forEach((line) => {
+    const [x, y] = line.split(/\s+/).map(Number);
+    a.push(x);
+    b.push(y);
+  });
+  a.sort();
+  b.sort();
+  for (let i = 0; i < lines.length; i++) {
+    totalDistance += Math.abs(a[i] - b[i]);
+  }
+  return totalDistance;
 }
